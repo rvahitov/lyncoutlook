4f6cb4f761a9d7085dfc4a20dd92775524ac06aa
rvahitov <rvahitov@outlook.com>
2015-10-03 18:14:33 +0300

    реализован метод Transform

--- src/OutlookLyncAddin.Common/SimpleMessageTransformer.cs
+++ src/OutlookLyncAddin.Common/SimpleMessageTransformer.cs
@@ -1,17 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace OutlookLyncAddin.Common
 {
     public class SimpleMessageTransformer: IMessageTransformer
     {
+        private readonly Regex[] _patterns;
+
         public SimpleMessageTransformer(IEnumerable<string> patterns)
         {
-
+            _patterns = patterns.Select(RegexFromPatternBuilder.Build).ToArray();
         }
 
         public string Transform(string message)
         {
-            throw new System.NotImplementedException();
+            MatchEvaluator replacement = m => string.Format("<a href=\"tel:{0}\">{0}</a>", m.Value);
+            return _patterns.Aggregate(message, (current, pattern) => pattern.Replace(current, replacement));
         }
     }
+
 }
