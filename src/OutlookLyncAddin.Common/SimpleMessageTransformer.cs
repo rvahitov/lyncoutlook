8870276732510a213c08c661a7649f2c5c19ba19
rvahitov <rvahitov@outlook.com>
2015-10-03 18:02:11 +0300

    Добавлен класс SimpleMessageTransformer

---
+++ src/OutlookLyncAddin.Common/SimpleMessageTransformer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace OutlookLyncAddin.Common
+{
+    public class SimpleMessageTransformer: IMessageTransformer
+    {
+        public SimpleMessageTransformer(IEnumerable<string> patterns)
+        {
+
+        }
+
+        public string Transform(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
