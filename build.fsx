435b03df5c6918cd2ee3c655eaad91aa9db7c9d4
Рамиль Вахитов <rvahitov@outlook.com>
2015-10-10 19:19:30 -0700

    Отредактирован build script

--- build.fsx
+++ build.fsx
@@ -1,1 +1,48 @@
-﻿
+﻿#r "./packages/fake/tools/fakelib.dll"
+
+open Fake
+open Fake.Testing
+
+let sourceDir = "./src/"
+
+let buildDir = "./build/"
+
+let testsSourceDir = "./tests/"
+
+let testsBuildDir = buildDir + "tests/"
+
+let deployDir = buildDir + "/setup/"
+
+MSBuildDefaults <- {MSBuildDefaults with ToolsVersion = Some("12.0")}
+
+Target "Clean" (fun _ ->
+    CleanDirs [buildDir]
+)
+
+Target "Build" (fun _ ->
+    !!(sourceDir + "**/*.csproj")
+    |> MSBuildDebug  buildDir "Build" 
+    |> Log "Build-Output: "
+)
+
+Target "TestBUild" (fun _ ->
+    !!(testsSourceDir + "**/*.csproj")
+    |> MSBuildDebug  testsBuildDir "Build" 
+    |> Log "BuildTest-Output: "
+)
+
+Target "Test" (fun _ -> 
+    !! (testsBuildDir + "*Tests.dll")
+    |> xUnit2 (fun p -> {p with HtmlOutputPath = Some(testsBuildDir + "test.html"); ToolPath = "packages/xunit.runner.console/tools/xunit.console.exe"})
+)
+
+Target "Deploy" (fun _ ->
+    !!(sourceDir + "**/*.wixproj")
+    |> MSBuildDebug  deployDir "Build" 
+    |> Log "BuildSetup-Output: "
+)
+
+
+"Clean" ==> "Build" ==> "TestBuild" ==> "Test" ==> "Deploy"
+
+RunTargetOrDefault "Deploy"
