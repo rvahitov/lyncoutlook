ac463d74381b71ccd2ec9cfb9415cdbf14b95958
Рамиль Вахитов <rvahitov@outlook.com>
2015-10-11 14:14:01 -0700

    Рефакторинг: классы из одного файла перемещенны в соответствующие файлы

---
+++ src/OutlookLyncAddin.Common/Configuration/PhonePatternCollectionConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OutlookLyncAddin.Common.Configuration
+{
+    [Serializable]
+    public class PhonePatternCollectionConfig : List<PhonePatternConfig>
+    {
+        
+    }
+}
--- src/OutlookLyncAddin.Common/Configuration/PhonePatternConfig.cs
+++ src/OutlookLyncAddin.Common/Configuration/PhonePatternConfig.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Xml;
 using System.Xml.Serialization;
 
 namespace OutlookLyncAddin.Common.Configuration
@@ -13,42 +10,5 @@ namespace OutlookLyncAddin.Common.Configuration
         public string Value { get; set; }
         [XmlAttribute("regex")]
         public bool IsRegex { get; set; }
-    }
-
-    [Serializable]
-    public class PhonePatternCollectionConfig : List<PhonePatternConfig>
-    {
-        
-    }
-
-    [Serializable,XmlRoot("outlookLyncAddin")]
-    public class OutlookLyncAddinConfig
-    {
-        public OutlookLyncAddinConfig()
-        {
-            Patterns = new PhonePatternCollectionConfig();
-        }
-
-        [XmlArray("patterrns"),XmlArrayItem("pattern")]
-        public PhonePatternCollectionConfig Patterns { get; set; }
-
-        public static OutlookLyncAddinConfig FromXml(string xmlPath)
-        {
-            using (var xmlReader = XmlReader.Create(xmlPath))
-            {
-                var xmlSerializer = new XmlSerializer(typeof(OutlookLyncAddinConfig));
-                return (OutlookLyncAddinConfig) xmlSerializer.Deserialize(xmlReader);
-            }
-        }
-
-        public static void ToXml(OutlookLyncAddinConfig config, string xmlPath)
-        {
-            using (var xmlWriter = XmlWriter.Create(xmlPath,new XmlWriterSettings{Encoding = Encoding.UTF8}))
-            {
-                var xmlSerializer = new XmlSerializer(typeof(OutlookLyncAddinConfig));
-                xmlSerializer.Serialize(xmlWriter,config);
-                xmlWriter.Flush();
-            }
-        }
     }
 }
